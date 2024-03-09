777b31a6426e40d8aec2c9347ee0ee161a36fc2a
Sam Waters <samuel.waters@gmail.com>
2024-03-09 09:11:08 -0700

    added base

---
+++ server/app.py
@@ -0,0 +1,24 @@
+from models import db
+from flask_restful import Api, Resource
+from flask_migrate import Migrate
+from flask import Flask, make_response, jsonify, request
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATABASE = os.environ.get(
+    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+api = Api(app)
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
