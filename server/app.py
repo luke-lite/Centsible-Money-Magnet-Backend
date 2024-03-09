7bddab30466af48c6a0995c70f940ff8d5f89c90
Sam Waters <samuel.waters@gmail.com>
2024-03-09 13:45:19 -0700

    added user and config

--- server/app.py
+++ server/app.py
@@ -1,23 +1,10 @@
-from models import db
-from flask_restful import Api, Resource
-from flask_migrate import Migrate
-from flask import Flask, make_response, jsonify, request
-import os
+from flask import request, make_response, session
+from flask_restful import Resource
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASE = os.environ.get(
-    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
-
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-
-migrate = Migrate(app, db)
-
-db.init_app(app)
-api = Api(app)
+# Local imports
+from config import app, db, api
+# Add your model imports
+# from models import 
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -1,17 +1,30 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
-from sqlalchemy.orm import validates, relationship
+from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.hybrid import hybrid_property
 
-convention = {
-    "ix": "ix_%(column_0_label)s",
-    "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "ck": "ck_%(table_name)s_%(constraint_name)s",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s"
-}
 
-metadata = MetaData(naming_convention=convention)
+from config import db, bcrypt
 
-db = SQLAlchemy(metadata=metadata)
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users_table'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable = False, unique = True)
+    email = db.Column(db.String)
+    _password_hash = db.Column(db.String, nullable=False)
+
+
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+
+    @password_hash.setter
+    def password_hash(self, password):
+        # utf-8 encoding and decoding is required in python 3
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
