0dfd59d52dd99d9d3cf0e2b7afb342adb5d14b36
luke-lite <105479902+luke-lite@users.noreply.github.com>
2024-03-11 10:45:18 -0400

    add goals and monthly_expenses

--- server/models.py
+++ server/models.py
@@ -74,3 +74,34 @@ class Categories(db.Model, SerializerMixin):
     category_description = db.Column(db.String, nullable=False)
     category_type = db.Column(db.Boolean, nullable=False)
 
+class Goals(db.Model, SerializerMixin):
+    # using specific table names for now
+    __tablename__ = 'goals_table'
+
+    id = db.Column(db.Integer, primary_key=True)
+    household_budget = db.Column(db.Boolean, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    target_amount = db.Column(db.String, nullable=False)
+    current_amount = db.Column(db.String, nullable=False)
+    deadline = db.Column(db.String, nullable=False)
+
+    #foreign keys
+    user_id = db.Column(db.String, db.Foreignkey("user_table.id"))
+    household_id = db.Column(db.String, db.Foreignkey("household_table.id"))
+
+class MonthlyExpenses(db.Model, SerializerMixin):
+    # using specific table names for now
+    __tablename__ = 'monthly_expenses_table'
+
+    # both foreign and primary?
+    id = db.Column(db.Integer, db.Foreignkey("expense_item_table.monthly_expenses_id"), primary_key=True)
+    is_household_budget = db.Column(db.Boolean, nullable=False)
+    user_expected_income = db.Column(db.String, nullable=False)
+    actual_income = db.Column(db.String, nullable=False)
+    user_expected_monthly_expenses_total = db.Column(db.String, nullable=False)
+    is_fluctuating_income = db.Column(db.Boolean, nullable=False)
+
+    #foreign keys
+    user_id = db.Column(db.String, db.Foreignkey("user_table.id"))
+    household_id = db.Column(db.String, db.Foreignkey("household_table.id"))
